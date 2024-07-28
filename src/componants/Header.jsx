049c2f9bc0def0d7c4c05bd6b00a4d2aff07807f
Yusuf-Hirsi <yusufhassanhirsi123@gmail.com>
2024-07-28 00:47:50 -0700

    bok note ract ah

--- src/componants/Header.jsx
+++ src/componants/Header.jsx
@@ -12,9 +12,14 @@ function  Header(){
     return <div>
     <div  className= " p-4 bg-purple-500">
          <div className=" flex justify-between">
+           
             <h1 className="text-3xl text-white">NOOTY</h1>
             <button onClick={handeleOpen} className="bg-orange-400 rounded p-5">Add NOTE</button>
          </div>
+    </div>
+    <div>
+    <img className="w-full h-screen" src="https://img.freepik.com/premium-photo/grocery-cart-with-colourful-gift-boxes-black-friday_175994-21320.jpg?w=826" alt="" />
+   
     </div>
          {
             isOpen ==true ?  <Form close={handelclose} /> :""
